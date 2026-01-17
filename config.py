4337b6b7d80729de558083c5fceb0d59e2d9302f
dvuvud <carldavid.buhrman@gmail.com>
2026-01-17 07:59:54 +0100

    add a config file for configuration settings

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+
+# Application settings
+APP_NAME = "mimizu"
+VERSION = "0.1.0"
+
+# Current default (might change)
+ACTIVATION_HOTKEY = '<ctrl>+<shift>+t'
+
+EASYOCR_PATH = 'vendor/easyocr'
+
+# GUI settings for lookups
+RESULT_WINDOW_WIDTH = 400
+RESULT_WINDOW_HEIGHT = 300
+RESULT_WINDOW_ALPHA = 0.95
+
+# Dictionary settings
+DICT_DIR = os.path.join(os.path.dirname(__file__), 'data', 'dictionaries')
